0f40ac0bc0e8ed1d49b23bd397684ed4df98c31b
kx0101 <liakos.koulaxis@yahoo.com>
2022-08-30 15:00:04 +0300

    nvim is awesome

---
+++ after/plugin/test.tsx
@@ -0,0 +1,11 @@
+import react, { useState } from 'react'
+
+export default App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+  <div>
+    <h2>Hello</h2>
+  </div>
+      )
+}
---
+++ init.lua
@@ -0,0 +1,24 @@
+require('base')
+require('highlights')
+require('maps')
+require('plugins')
+
+vim.cmd('nnoremap <C-q> :NERDTreeFocus<CR>')
+vim.cmd('nnoremap <C-e> :NERDTreeToggle<CR>')
+vim.cmd('nnoremap <C-p> :Telescope find_files<CR>')
+vim.cmd('nnoremap <C-y> :Telescope live_grep<CR>')
+vim.cmd([[nmap <C-z> <Nop>]])
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('macos')
+end
+if is_win then
+  require('windows')
+end
